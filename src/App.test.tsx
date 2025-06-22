bb4bd31856d6735473c3cdf276601863a844a915
hamilgdev <hamilgdev@gmail.com>
2025-06-22 12:57:35 -0500

    feat: enhance App tests to include sign-in page rendering and mock authentication services

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,26 @@
 import { screen, render } from '@testing-library/react';
+import { vi } from 'vitest';
 import App from './App';
 
+vi.mock('@/services/auth.service', () => ({
+  signInWithEmail: vi.fn(),
+  refreshToken: vi.fn(),
+}));
+
+vi.mock('@/handlers/browser-storage.handler', () => ({
+  getLocal: vi.fn(),
+  setLocal: vi.fn(),
+  removeLocal: vi.fn(),
+}));
+
 describe('App tests', () => {
-  it('should render the title', () => {
+  it('should render the application and redirect to sign-in page', () => {
     render(<App />);
 
+    expect(screen.getByLabelText(/correo electrónico/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/contraseña/i)).toBeInTheDocument();
     expect(
-      screen.getByRole('heading', {
-        level: 1,
-      })
-    ).toHaveTextContent('Hello 🐼!');
+      screen.getByRole('button', { name: /iniciar sesión/i })
+    ).toBeInTheDocument();
   });
 });
